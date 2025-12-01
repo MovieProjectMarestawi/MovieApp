bf08d270da634ba35b542b295abd94f95bf8dd2e
Jejepu <jere.puirava@gmail.com>
2025-12-01 13:21:36 +0200

    ui components

---
+++ client/src/components/ui/alert-dialog.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import * as React from "react";
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
+
+import { cn } from "./utils";
+import { buttonVariants } from "./button";
+
+function AlertDialog({
+  ...props
+}: React.ComponentProps<typeof AlertDialogPrimitive.Root>) {
+  return <AlertDialogPrimitive.Root data-slot="alert-dialog" {...props} />;
+}
+
+function AlertDialogTrigger({
+    ...props
+}: React.ComponentProps<typeof AlertDialogPrimitive.Trigger>) {
+    return <
+}
+}
